c3a6063d0ebc7c7bce2830dd18615222b26a217b
luckydela <luckydela93@gmail.com>
2023-06-13 09:20:00 +0000

    dfhofief

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -24,7 +24,7 @@ const Topbar = () => {
 
             {/* <Settings /> */}
           </div>
-          <i class="fa-light fa-user"></i>
+          <i class="fa fa-user"></i>
           {/* <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" /> */}
         </div>
       </div>

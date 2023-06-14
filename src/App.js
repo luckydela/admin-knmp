340d9a515bdebf4d6a514028f53ac5680cefc8be
MANDELA <mandela@MANDELAs-MacBook-Pro.local>
2023-06-14 12:20:05 +0000

    this checkor login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Applayout from './components/layout/Applayout';
+import Cklogin from './views/checkor/login/Cklogin';
 
 function App() {
   return (
    <Router>
      <Routes>
      <Route path="*" name="dashboard" element={<Applayout />} />
+     <Route path="/" name="checker login" element={<Cklogin />} />
+
      </Routes>
    </Router>
   );
--- src/views/checkor/login/Cklogin.jsx
+++ src/views/checkor/login/Cklogin.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './cklogin.css' 
+import loginimg from '../../../assets/cklogin.jpeg'
 
 const Cklogin = () => {
   return (
-    <div>Cklogin</div>
+    
+    <section class="login">
+		<div class="login_box">
+			<div class="left">
+			
+				<div class="contact">
+					<form action="">
+						<h3>Login</h3>
+						<input type="text" placeholder="USERNAME"/>
+						<input type="text" placeholder="PASSWORD"/>
+						<button class="submit">LET'S GO</button>
+					</form>
+			</div>
+			<div class="right">
+				<div class="right-text">
+					<h2>LONYX</h2>
+					<h5>A UX BASED CREATIVE AGENCEY</h5>
+				</div>
+				<div class="right-inductor">
+          <img src={loginimg} alt=""/>
+        </div>
+			</div>
+		</div>
+	</div>
+  </section>
   )
 }
 
